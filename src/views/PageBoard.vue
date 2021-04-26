a595ece773572ff0b60889a9ceb8f539b600fbb5
Masa <katsukatsu.masa.0522@gmail.com>
2021-04-26 21:03:23 +0900

    first commit

---
+++ src/views/PageBoard.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <v-row justify="start">
+      <v-col cols="auto">
+        <h1 class="ma-3">{{ $store.state.user.user.name }}'s Boards</h1>
+      </v-col>
+    </v-row>
+    <the-boards />
+  </v-container>
+</template>
+
+<script>
+import TheBoards from '@/components/TheBoards.vue';
+export default {
+  name: 'PageBoards',
+  components: {
+    TheBoards,
+  },
+};
+</script>
